dfdf81d6f39ff24c7c25a464cb3eea08580efc46
Chibuikem91 <108734689+Chibuikem91@users.noreply.github.com>
2023-02-17 08:07:01 -0800

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdion>
+
+/**
+ * main - prints the lowercase alphabets in reverse
+ * Return: Always 0
+ */
+int main(void)
+{
+	char le;
+
+	for (le = 'z'; le >= 'a'; ch--)
+		putchar(le);
+
+	putchar('\n');
+
+	return (0);
+}

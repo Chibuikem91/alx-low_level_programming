ab29a7411c73257b9fd0200d7bcc42e758aa2082
Chibuikem91 <108734689+Chibuikem91@users.noreply.github.com>
2023-02-17 07:54:14 -0800

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints sigble digits in base 10
+ * Return: Always 0
+ */
+int main(void)
+{
+	int numb;
+
+	for (num = 0; num < 10; num++)
+		putchar ((num % 10) + '0');
+
+	putchar('n\');
+
+	return (0);
+}

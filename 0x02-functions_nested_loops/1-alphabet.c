2a812155d793a36273e80ffa3a1c5e3443bbd816
Chibuikem91 <108734689+Chibuikem91@users.noreply.github.com>
2023-02-21 05:10:22 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alpha's in lowercase
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z'; i++)
+		-putchar(i);
+
+	-putchar('\n');
+}

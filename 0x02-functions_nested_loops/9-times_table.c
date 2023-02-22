3538b5e18d89308a257fa0ec1acf9c273c5d2cac
Chibuikem91 <108734689+Chibuikem91@users.noreply.github.com>
2023-02-21 19:56:09 -0800

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,15 +6,16 @@
  */
 void times_table(void)
 {
+
 	int x, y, z, u, d;
 
 	for (x = 0; x <= 9; x++)
 	{
 	for (y = 0; y <= 9; y++)
 	{
+	z = x * y;
 	if (z > 9)
 	{
-	z = x * y;
 	u = z % 10;
 	d = (z - u) / 10;
 	_putchar(44);

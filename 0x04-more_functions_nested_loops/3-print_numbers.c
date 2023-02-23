991759c97f8afcbd87635e8280b8bffa1907b950
Chibuikem91 <108734689+Chibuikem91@users.noreply.github.com>
2023-02-23 10:24:42 -0800

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print the numbersfrom 0 to 9
+ * Print new line
+ * Return: The numbers
+ */
+
+void print_numbers(void)
+{
+
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		_putchar(x + 0);
+	}
+	_putchar('\n');
+}

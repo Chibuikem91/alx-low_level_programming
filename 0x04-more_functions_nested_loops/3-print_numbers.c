aa596b94efaf5d4b3d646028b546e7fb5be64862
Chibuikem91 <108734689+Chibuikem91@users.noreply.github.com>
2023-02-23 10:27:24 -0800

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_numbers - Print the numbersfrom 0 to 9
+ * print_numbers - Print the numbers since 0 up to 9
  * Print new line
- * Return: The numbers
+ * Return: The numbers since 0 up to 9
  */
 
 void print_numbers(void)
@@ -13,7 +13,7 @@ void print_numbers(void)
 
 	for (x = 0; x <= 9; x++)
 	{
-		_putchar(x + 0);
+		_putchar(x + '0');
 	}
 	_putchar('\n');
 }

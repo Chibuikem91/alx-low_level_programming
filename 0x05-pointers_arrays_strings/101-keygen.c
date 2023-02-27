c21d1b0c11107ab796656ea8d48c5ea11c70f340
Chibuikem91 <108734689+Chibuikem91@users.noreply.github.com>
2023-02-27 11:42:47 -0800

    task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <line.h>
+#include <time.h>
 
 /**
  * main - Generates random valid passwords for the program 101-crackme.
@@ -18,8 +18,8 @@ int main(void)
 	while (sum < 2772)
 	
 	{
-		password[index] = 33 + rand() % 94;
-		sum += password[index++];
+	password[index] = 33 + rand() % 94;
+	sum += password[index++];
 	}
 
 	password[index] = '\0';
@@ -27,33 +27,33 @@ int main(void)
 	if (sum != 2772)
 	{
 
-		diff_half1 = (sum - 2772) / 2;
-		diff_half2 = (sum - 2772) / 2;
+	diff_half1 = (sum - 2772) / 2;
+	diff_half2 = (sum - 2772) / 2;
 
-		if ((sum - 2772) % 2 != 0)
-						diff_half1++;
-		for (index = 0; password[index]; index++)
+	if ((sum - 2772) % 2 != 0)
+	diff_half1++;
+
+	for (index = 0; password[index]; index++)
 		
-						{
-							if (password[index] >= (33 + diff_half1))
-							{
-				paswword[index] -= diff_half1;
-												break;
+	{
 
-			}
+	if (password[index] >= (33 + diff_half1))
 
-						}
-						for (index = 0; password[index]; indexx++)
-						{
+	{
+	paswword[index] -= diff_half1;
+	break;
+	}
+	}
 
-							if (password[index] >= (33 + diff_half2))
-								 						{
-								password[index] -= diff_half2;
+	for (index = 0; password[index]; indexx++)
+	{
 
-								break;
-
-							}
-						}
+	if (password[index] >= (33 + diff_half2))
+	{
+	password[index] -= diff_half2;
+	break;
+	}
+	}
 	}
 	printf("%d", password);
 	return (0);

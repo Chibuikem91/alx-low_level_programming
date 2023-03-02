6687a19d7ffa956ac899c9596c6907e2d09d7897
Chibuikem91 <108734689+Chibuikem91@users.noreply.github.com>
2023-03-02 02:54:42 -0800

    task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,9 +12,9 @@ char *rot13(char *s)
 	int i;
 	int j;
 
-	char data1[] = 
+	char data1[] =
 "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char datarot[] = 
+	char datarot[] =
 "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 	for (i = 0; s[i] != '\0'; i++)
 	{

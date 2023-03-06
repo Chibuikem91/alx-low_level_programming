90e938717bd1c435dc62b2c797bc24b2d40cc6cc
Chibuikem91 <108734689+Chibuikem91@users.noreply.github.com>
2023-03-06 14:01:10 -0800

    task 0-101

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,15 +7,11 @@
  * @n: max bytes to use
  * Return: s
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; n > 0; i++, n--)
-	{
+	for (i = 0; i < n; i++)
 		s[i] = b;
-	}
-
 	return (s);
 }

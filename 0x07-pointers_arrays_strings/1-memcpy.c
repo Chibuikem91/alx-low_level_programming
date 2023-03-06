20166701b337e2359b3c404cb5620628d9903794
Chibuikem91 <108734689+Chibuikem91@users.noreply.github.com>
2023-03-06 13:20:30 -0800

    task 0-101

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,15 +6,14 @@
  * @src: source
  * @n: length of src
  *
- * Return: pointer to dest.
+ * Return: Always 0.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-	{
 		dest[i] = src[i];
-	}
+
 	return (dest);
 }

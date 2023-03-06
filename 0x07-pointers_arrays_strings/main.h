9a8c1d636ae77058612185a25efb8925cc43ee5e
Chibuikem91 <108734689+Chibuikem91@users.noreply.github.com>
2023-03-06 14:21:35 -0800

    task 0-101

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 int _putchar(char c);
-void *_memcpy(void *dest, const void *src, size_t n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);

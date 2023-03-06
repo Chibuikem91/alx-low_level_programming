979413ae3b13fcf39889f052c04c48a22c872125
Chibuikem91 <108734689+Chibuikem91@users.noreply.github.com>
2023-03-06 13:54:46 -0800

    task 0-101

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,15 +5,26 @@
  * @s: pointer to put the constant
  * @b: constant byte
  * @n: bytes
- * Return: Always 0
+ * Return: Nothing
  */
 
 void simple_print_buffer(char *buffer, unsigned int size)
 {
 	unsigned int i;
 
-	for (i = 0; i < n; i++)
-		s[i] = b;
-
-	return (s);
+	i = 0;
+	while (i < size)
+	{
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i %10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", buffer[i]);
+		i++;
+	}
+	printf("\n");
 }
